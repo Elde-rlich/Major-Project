39e31c47d101f830998b610fc0479eb933ca9581
Elder-lich <sushanthbende@gmail.com>
2025-09-09 23:11:36 +0530

    isolating urls for debugging, made debug = true

--- fashintel/urls.py
+++ fashintel/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def simple_test(request):
+    return HttpResponse("✅ Django is working! Fashion Recommender is online.")
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #path('auth/', include('authapp.urls')),
     #path('catalog/', include('recommender.urls')),
-    path('', include('landing.urls')),
+    #path('', include('landing.urls')),
+    path('', simple_test, name = 'test')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
