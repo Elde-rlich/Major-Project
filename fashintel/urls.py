9536331cac9f4309c306e767b2071c537a846b43
Elder-lich <sushanthbende@gmail.com>
2025-09-09 23:04:28 +0530

    isolating urls for debugging

--- fashintel/urls.py
+++ fashintel/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('authapp.urls')),
-    path('catalog/', include('recommender.urls')),
+    #path('admin/', admin.site.urls),
+    #path('auth/', include('authapp.urls')),
+    #path('catalog/', include('recommender.urls')),
     path('', include('landing.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
