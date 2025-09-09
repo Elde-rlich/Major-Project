33c5378454a9ec2381a88cce573eadab2dea26c6
Elder-lich <sushanthbende@gmail.com>
2025-09-09 23:24:55 +0530

    Tried with all urls again

--- fashintel/urls.py
+++ fashintel/urls.py
@@ -18,15 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-
-def simple_test(request):
-    return HttpResponse("✅ Django is working! Fashion Recommender is online.")
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    #path('auth/', include('authapp.urls')),
-    #path('catalog/', include('recommender.urls')),
-    #path('', include('landing.urls')),
-    path('', simple_test, name = 'test')
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('auth/', include('authapp.urls')),
+    path('catalog/', include('recommender.urls')),
+    path('', include('landing.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
